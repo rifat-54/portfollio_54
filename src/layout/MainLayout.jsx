6895a5b120268782ae1aa7199f0c08e195be3b3c
rifat-54 <rifat999por@gmail.com>
2025-04-11 07:16:23 +0600

    first commit

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainLayout = () => {
+    return (
+        <div>
+            man layout
+        </div>
+    );
+};
+
+export default MainLayout;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+
+
+
+const routes=createBrowserRouter([
+    {
+        path:'/',
+        element:<MainLayout></MainLayout>  
+    }
+])
+
+export default routes;
