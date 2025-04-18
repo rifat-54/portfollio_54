c46a654f2df2d54a1aaa844284f0fe2385d46cba
rifat-54 <rifat999por@gmail.com>
2025-04-18 11:51:31 +0600

    addeds projects section

--- src/pages/home/homeComponent/projects/Projects.jsx
+++ src/pages/home/homeComponent/projects/Projects.jsx
@@ -1,44 +1,42 @@
-import React from 'react';
+import React from "react";
 // import React, { useRef, useState } from 'react';
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import './project.css'
-
+import "swiper/css";
+import "swiper/css/navigation";
+import "./project.css";
 
 // import required modules
-import { Navigation } from 'swiper/modules';
-import Slider from './Slider';
+import { Navigation } from "swiper/modules";
+import Slider from "./Slider";
 const Projects = () => {
-    return (
-        <div>
-          <h2 className='text-3xl font-bold text-center mt-2 mb-8'>Popular Project</h2>
-              <>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-       {/* project 1 */}
-        <SwiperSlide>
-          <Slider
-          img={'https://i.ibb.co.com/BWkn5nJ/Screenshot-19.png'}
-          url={'https://bistro-boss-restaurant-42687.web.app/'}
-          ></Slider>
+  return (
+    <div>
+      <h2 className="text-3xl font-bold text-center mt-2 mb-8">
+        Popular Project
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+        <div className="card bg-base-100  shadow-sm">
+          <figure>
+            <img
+              src="https://i.ibb.co.com/BWkn5nJ/Screenshot-19.png"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body text-black">
+            <h2 className="text-2xl font-semibold text-center">Bistro Boss Restaurant</h2>
             
-        </SwiperSlide>
-        {/* project 2 */}
-        <SwiperSlide>
-          <Slider
-          img={'https://i.ibb.co.com/TB8nvQ5y/Screenshot-21.png'}
-          url={'https://a-12-learnnest.web.app/'}
-          ></Slider>
-            
-        </SwiperSlide>
-        
-      </Swiper>
-    </>
+            <div className="card-actions justify-center">
+              <button className="btn btn-primary">See More</button>
+            </div>
+          </div>
         </div>
-    );
+        
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
---
+++ src/pages/home/homeComponent/projects/Swiperslider.jsx
@@ -0,0 +1,21 @@
+<>
+<Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+ {/* project 1 */}
+  <SwiperSlide>
+    <Slider
+    img={'https://i.ibb.co.com/BWkn5nJ/Screenshot-19.png'}
+    url={'https://bistro-boss-restaurant-42687.web.app/'}
+    ></Slider>
+      
+  </SwiperSlide>
+  {/* project 2 */}
+  <SwiperSlide>
+    <Slider
+    img={'https://i.ibb.co.com/TB8nvQ5y/Screenshot-21.png'}
+    url={'https://a-12-learnnest.web.app/'}
+    ></Slider>
+      
+  </SwiperSlide>
+  
+</Swiper>
+</>
